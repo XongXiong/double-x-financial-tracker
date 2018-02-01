7fb8a7d370ed443267e4999efd70b70dd9b9911b
Xong Xiong <xxiong3153@gmail.com>
2018-02-01 00:04:11 -0600

    trying to get angular ui working

--- server/public/scripts/controllers/transactions.controller.js
+++ server/public/scripts/controllers/transactions.controller.js
@@ -1,5 +1,21 @@
-myApp.controller('TransactionsController', function (UserService) {
+myApp.controller('TransactionsController', function (UserService, $scope) {
         console.log('TransactionsController created');
         var vm = this;
-        vm.userService = UserService;
+    vm.userService = UserService;
+    
+$scope.items = ['The first choice!', 'And another choice for you.', 'but wait! A third!'];
+
+$scope.status = {
+    isopen: false
+};
+
+$scope.toggled = function (open) {
+    $log.log('Dropdown is now: ', open);
+};
+
+$scope.toggleDropdown = function ($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+};
     });
