e6e63317cecbbbfdf886be1042a55dfe5420daf7
Xong Xiong <xxiong3153@gmail.com>
2018-02-09 01:59:34 -0600

    added dropdown

--- server/public/scripts/controllers/transactions.controller.js
+++ server/public/scripts/controllers/transactions.controller.js
@@ -1,21 +1,11 @@
-myApp.controller('TransactionsController', function (UserService, $scope) {
+myApp
+    .controller('TransactionsController', function (UserService) {
         console.log('TransactionsController created');
         var vm = this;
-    vm.userService = UserService;
-    
-$scope.items = ['The first choice!', 'And another choice for you.', 'but wait! A third!'];
+        vm.userService = UserService;
 
-$scope.status = {
-    isopen: false
-};
-
-$scope.toggled = function (open) {
-    $log.log('Dropdown is now: ', open);
-};
-
-$scope.toggleDropdown = function ($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
+vm.openMenu = function ($mdMenu, ev) {
+    originatorEv = ev;
+    $mdMenu.open(ev);
 };
     });
